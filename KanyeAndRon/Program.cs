b0d8be307a61c54c6bcd8f89a6cec0a17f0fbb91
Ben Zellers <zellersdenver@gmail.com>
2023-06-15 15:38:10 -0400

    KanyeandRon

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KanyeAndRon;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices.JavaScript;
+
+for(int i = 0; i < 5; i++)
+{
+    
+    DontQuoteMe.KQuote();
+
+    DontQuoteMe.RQoute();
+
+}
+
+
